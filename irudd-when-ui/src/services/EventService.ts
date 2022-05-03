21bf40ab0ae6e133ab3adecfb694ea9f02268767
Andreas Lundgren <andreas.lundgren@naktergaltech.se>
2022-05-03 07:57:54 +0200

    api works

--- irudd-when-ui/src/services/EventService.ts
+++ irudd-when-ui/src/services/EventService.ts
@@ -1,31 +1,94 @@
 import { CreateEventState } from "../features/createEvent/createEventSlice";
 import { ExistingEvent } from "../features/currentEvent/currentEventSlice";
 
-const MockDelayMs = 200;
+interface IEventService {
+    createNewEvent(data: CreateEventState) : Promise<ExistingEvent>;
+    loadExistingEvent(eventId: string) : Promise<ExistingEvent | null>;
+}
 
-
-/*
-const response = window.fetch('https://localhost:7095/api/v1/create-event', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json;charset=UTF-8',
-    },
-    body: JSON.stringify({ id: 'test' }),
-  })
-*/
-
-/*
-const response = window.fetch('https://localhost:7095/api/v1/event/A424224', {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json;charset=UTF-8',
+function getEventService() : IEventService {
+    if(process.env.REACT_APP_API_URL) {
+        return new ApiEventService(process.env.REACT_APP_API_URL);
+    } else {
+        return new LocalStorageEventService('event_v1_');
     }
-  })
-*/
+}
 
+//This is a class since I could not get jest.spyOn to work with exporting getEventService and mocking that which is what I wanted to do.
+export default class EventService implements IEventService {
+    private service: IEventService;
 
-//TODO: Replace with an actual backend
-export default class EventService {
+    constructor() {
+        this.service = getEventService();
+        this.createNewEvent = this.createNewEvent.bind(this);
+        this.loadExistingEvent = this.loadExistingEvent.bind(this);
+    }
+
+    createNewEvent(data: CreateEventState) : Promise<ExistingEvent> {
+        return this.service.createNewEvent(data);
+    }
+
+    loadExistingEvent(eventId: string) : Promise<ExistingEvent | null> {
+        return this.service.loadExistingEvent(eventId);
+    }
+}
+
+class ApiEventService implements IEventService {
+    constructor(private rootUrl: string) {
+        if(!rootUrl.startsWith('https')) {
+            throw new Error('rootUrl must be https://<...>')
+        }
+        this.getAbsoluteUrl = this.getAbsoluteUrl.bind(this);
+        this.createNewEvent = this.createNewEvent.bind(this);
+    }
+
+    async createNewEvent(data: CreateEventState) : Promise<ExistingEvent> {
+        let response = await window.fetch(this.getAbsoluteUrl('api/v1/create-event'), {
+            method: 'POST',
+            headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            },
+            body: JSON.stringify(data),
+        });
+        if(!response.ok) {
+            throw new Error(response.statusText)
+        }
+        let result = (await response.json()) as ExistingEvent
+        return result
+    }
+
+    async loadExistingEvent(eventId: string) : Promise<ExistingEvent | null> {
+        let response = await window.fetch(this.getAbsoluteUrl(`api/v1/event/${eventId}`), {
+            method: 'GET',
+            headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            }
+        })
+        if(response.status === 404) {
+            return null;
+        } else if(!response.ok) {
+            throw new Error(response.statusText)
+        } else {
+            let result = (await response.json()) as ExistingEvent;
+            return result;
+        }
+    }
+
+    private getAbsoluteUrl(relativeUrl: string) {
+        let url = this.rootUrl
+        if(!url.endsWith('/')) {
+            url += '/'
+        }
+        url += relativeUrl.startsWith('/') ? relativeUrl.substring(1) : relativeUrl;
+        return url;
+    }
+}
+
+const MockDelayMs = 50;
+class LocalStorageEventService implements IEventService {
+    constructor(private storagePrefix: string) {
+
+    }
     createNewEvent(data: CreateEventState) : Promise<ExistingEvent> {
         const generateItemId = () => Math.random().toString(36).substring(2, 8);
         return new Promise<ExistingEvent>(resolve => {
@@ -39,7 +102,7 @@ export default class EventService {
                     description: data.description
                 };
 
-                window.localStorage.setItem('event_' + eventId, JSON.stringify(event));
+                window.localStorage.setItem(this.storagePrefix + eventId, JSON.stringify(event));
 
                 resolve(event);
             }, MockDelayMs);
@@ -49,9 +112,8 @@ export default class EventService {
     loadExistingEvent(eventId: string) : Promise<ExistingEvent | null> {
         return new Promise<ExistingEvent | null>(resolve => {
             setTimeout(() => {
-                let rawData = window.localStorage.getItem('event_' + eventId);
+                let rawData = window.localStorage.getItem(this.storagePrefix + eventId);
                 if(rawData) {
-                    console.log(rawData)
                     resolve(JSON.parse(rawData));
                 } else {
                     resolve(null);
