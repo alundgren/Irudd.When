3b247b7c20f789d44e4e56ee2dc55b0ed72d61bd
Andreas Lundgren <andreas.lundgren@naktergaltech.se>
2022-05-03 08:56:57 +0200

    beta

--- irudd-when-ui/src/components/IconForButton.tsx
+++ irudd-when-ui/src/components/IconForButton.tsx
@@ -1,5 +1,5 @@
 export interface ButtonIconType {
-    iconType: 'addPerson' | 'removePerson' | 'addTime' | 'removeTime' | 'showCalendar'
+    iconType: 'addPerson' | 'removePerson' | 'addTime' | 'removeTime' | 'showCalendar' | 'pendingAnswer' | 'acceptedAnswer' | 'rejectedAnswer'
 }
 
 let addPersonIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-plus" viewBox="0 0 16 16">
@@ -21,6 +21,18 @@ let calendarWithPlusIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" he
         <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM8 8a.5.5 0 0 1 .5.5V10H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V11H6a.5.5 0 0 1 0-1h1.5V8.5A.5.5 0 0 1 8 8z"/>
     </svg>
 
+let questionMarkIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-question" viewBox="0 0 16 16">
+    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
+</svg>
+
+let checkIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2" viewBox="0 0 16 16">
+    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+</svg>
+
+let smallCrossIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+
 function IconForButton({iconType}: ButtonIconType) {
     if(iconType === 'addPerson')
         return addPersonIcon
@@ -30,6 +42,12 @@ function IconForButton({iconType}: ButtonIconType) {
         return plusIcon
     else if(iconType === 'showCalendar')
         return calendarWithPlusIcon
+    else if(iconType === 'pendingAnswer')
+        return questionMarkIcon
+    else if(iconType === 'acceptedAnswer')
+        return checkIcon
+    else if(iconType === 'rejectedAnswer')
+        return smallCrossIcon
     else
         return (null)
 }
--- irudd-when-ui/src/features/currentEvent/CurrentEvent.tsx
+++ irudd-when-ui/src/features/currentEvent/CurrentEvent.tsx
@@ -3,6 +3,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import EventService from "../../services/EventService";
 import { useParams } from "react-router-dom";
+import IconForButton from "../../components/IconForButton";
 
 export function CurrentEvent() {
     const currentEvent = useSelector((x : { currentEvent : CurrentEventState }) => x.currentEvent.event);
@@ -19,8 +20,64 @@ export function CurrentEvent() {
         }
     }, [eventId, currentEventId, dispatch])
     
-    const json = JSON.stringify(currentEvent);
+    //http://localhost:3000/event/e9a8wa18dh2sw
+    const json = JSON.stringify(currentEvent,null, ' ');
+    let containerStyle = {
+        
+    }
+    let timeColumnStyle = {
+        width: 100
+    }
     return (
-        <pre>{json}</pre>
+        <div style={{ outline:'1px solid black' }}>
+            <div className="d-flex flex-grow-1 p-2 flex-column" style={containerStyle}>
+                <div className="d-flex flex-grow-1 flex-row">
+                    <div style={timeColumnStyle}>
+                        2022-05-03
+                    </div>
+                    <div style={timeColumnStyle}>
+                        2022-05-04
+                    </div>
+                    <div style={timeColumnStyle}>
+                        2022-05-04
+                    </div>                 
+                </div>
+                <div className="d-flex flex-grow-1 flex-row">
+                    <div style={timeColumnStyle}>
+                        12:30
+                    </div>
+                    <div style={timeColumnStyle}>
+                        11:30
+                    </div>
+                    <div style={timeColumnStyle}>
+                        12:30
+                    </div>                 
+                </div>                            
+                <div className="d-flex flex-grow-1 flex-row">
+                    <div className="d-flex flex-grow-1 justify-content-center align-items-center">
+                        Karl Holger
+                    </div>
+                </div>
+                <div className="d-flex flex-grow-1 flex-row">
+                    <div style={timeColumnStyle}>
+                        <button className="btn btn-warning d-flex justify-content-center align-items-center" type="button">
+                            <IconForButton iconType='pendingAnswer' />
+                        </button>
+                    </div>
+                    <div style={timeColumnStyle}>
+                        <button className="btn btn-success d-flex justify-content-center align-items-center" type="button">
+                            <IconForButton iconType='acceptedAnswer' />
+                        </button>
+                    </div>
+                    <div style={timeColumnStyle}>
+                        <button className="btn btn-danger d-flex justify-content-center align-items-center" type="button">
+                            <IconForButton iconType='rejectedAnswer' />
+                        </button>
+                    </div>                 
+                </div>                  
+            </div>
+            <pre>{json}</pre>
+        </div>
+        
     );
 }
